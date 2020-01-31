Vitalii81/OrcaSlicerMini

commit 6b0de7a983eb1e07dffeb24de8a08b7f7da99517

Drain holes are now saved in ModelObject Internal changes in GLGizmoHollow.cpp

src/libslic3r/Model.cpp
@@ -606,6 +606,7 @@ ModelObject& ModelObject::assign_copy(const ModelObject &rhs)
     assert(this->config.id() == rhs.config.id());
     this->sla_support_points          = rhs.sla_support_points;
     this->sla_points_status           = rhs.sla_points_status;
+    this->sla_drain_holes             = rhs.sla_drain_holes;
     this->layer_config_ranges         = rhs.layer_config_ranges;    // #ys_FIXME_experiment
     this->layer_height_profile        = rhs.layer_height_profile;
     this->printable                   = rhs.printable;
@@ -646,6 +647,7 @@ ModelObject& ModelObject::assign_copy(ModelObject &&rhs)
     assert(this->config.id() == rhs.config.id());
     this->sla_support_points          = std::move(rhs.sla_support_points);
     this->sla_points_status           = std::move(rhs.sla_points_status);
+    this->sla_drain_holes             = std::move(rhs.sla_drain_holes);
     this->layer_config_ranges         = std::move(rhs.layer_config_ranges); // #ys_FIXME_experiment
     this->layer_height_profile        = std::move(rhs.layer_height_profile);
     this->origin_translation          = std::move(rhs.origin_translation);
@@ -1099,6 +1101,7 @@ ModelObjectPtrs ModelObject::cut(size_t instance, coordf_t z, bool keep_upper, b
     if (keep_upper) {
         upper->set_model(nullptr);
         upper->sla_support_points.clear();
+        lower->sla_drain_holes.clear();
         upper->sla_points_status = sla::PointsStatus::NoPoints;
         upper->clear_volumes();
         upper->input_file = "";
@@ -1107,6 +1110,7 @@ ModelObjectPtrs ModelObject::cut(size_t instance, coordf_t z, bool keep_upper, b
     if (keep_lower) {
         lower->set_model(nullptr);
         lower->sla_support_points.clear();
+        lower->sla_drain_holes.clear();
         lower->sla_points_status = sla::PointsStatus::NoPoints;
         lower->clear_volumes();
         lower->input_file = "";

src/libslic3r/Model.hpp
@@ -203,6 +203,9 @@ public:
     // the SLA gizmo and the backend).
     sla::PointsStatus sla_points_status = sla::PointsStatus::NoPoints;
 
+    // Holes to be drilled into the object so resin can flow out
+    std::vector<sla::DrainHole> sla_drain_holes;
+
     /* This vector accumulates the total translation applied to the object by the
         center_around_origin() method. Callers might want to apply the same translation
         to new volumes before adding them to this object in order to preserve alignment
@@ -372,7 +375,7 @@ private:
 	template<class Archive> void serialize(Archive &ar) {
 		ar(cereal::base_class<ObjectBase>(this));
 		Internal::StaticSerializationWrapper<ModelConfig> config_wrapper(config);
-        ar(name, input_file, instances, volumes, config_wrapper, layer_config_ranges, layer_height_profile, sla_support_points, sla_points_status, printable, origin_translation,
+        ar(name, input_file, instances, volumes, config_wrapper, layer_config_ranges, layer_height_profile, sla_support_points, sla_points_status, sla_drain_holes, printable, origin_translation,
             m_bounding_box, m_bounding_box_valid, m_raw_bounding_box, m_raw_bounding_box_valid, m_raw_mesh_bounding_box, m_raw_mesh_bounding_box_valid);
 	}
 };

src/libslic3r/SLA/SLACommon.hpp
@@ -80,6 +80,39 @@ struct SupportPoint
 
 using SupportPoints = std::vector<SupportPoint>;
 
+struct DrainHole
+{
+    Vec3f m_pos;
+    Vec3f m_normal;
+    float m_radius;
+    float m_height;
+
+    DrainHole()
+        : m_pos(Vec3f::Zero()), m_normal(Vec3f::UnitZ()), m_radius(5.f),
+          m_height(10.f)
+    {}
+
+    DrainHole(Vec3f position, Vec3f normal, float radius, float height)
+        : m_pos(position)
+        , m_normal(normal)
+        , m_radius(radius)
+        , m_height(height)
+    {}
+
+    bool operator==(const DrainHole &sp) const
+    {
+        return (m_pos == sp.m_pos) && (m_normal == sp.m_normal)
+             && is_approx(m_radius, sp.m_radius) && is_approx(m_height, sp.m_height);
+    }
+
+    bool operator!=(const DrainHole &sp) const { return !(sp == (*this)); }
+
+    template<class Archive> void serialize(Archive &ar)
+    {
+        ar(m_pos, m_normal, m_radius, m_height);
+    }
+};
+
 struct Contour3D;
 
 /// An index-triangle structure for libIGL functions. Also serves as an

src/slic3r/GUI/Gizmos/GLGizmoHollow.hpp
@@ -25,7 +25,7 @@ private:
     ObjectID m_model_object_id = 0;
     int m_active_instance = -1;
     float m_active_instance_bb_radius; // to cache the bb
-    mutable double m_z_shift = 0.f;
+    mutable double m_z_shift = 0.;
     bool unproject_on_mesh(const Vec2d& mouse_pos, std::pair<Vec3f, Vec3f>& pos_and_normal);
 
     const float RenderPointScale = 1.f;
@@ -46,27 +46,26 @@ private:
     class CacheEntry {
     public:
         CacheEntry() :
-            support_point(sla::SupportPoint()), selected(false), normal(Vec3f::Zero()) {}
+            drain_hole(sla::DrainHole()), selected(false) {}
 
-        CacheEntry(const sla::SupportPoint& point, bool sel = false, const Vec3f& norm = Vec3f::Zero()) :
-            support_point(point), selected(sel), normal(norm) {}
+        CacheEntry(const sla::DrainHole& point, bool sel = false) :
+            drain_hole(point), selected(sel) {}
 
         bool operator==(const CacheEntry& rhs) const {
-            return (support_point == rhs.support_point);
+            return (drain_hole == rhs.drain_hole);
         }
 
         bool operator!=(const CacheEntry& rhs) const {
             return ! ((*this) == rhs);
         }
 
-        sla::SupportPoint support_point;
+        sla::DrainHole drain_hole;
         bool selected; // whether the point is selected
-        Vec3f normal;
 
         template<class Archive>
         void serialize(Archive & ar)
         {
-            ar(support_point, selected, normal);
+            ar(drain_hole, selected);
         }
     };
 
@@ -97,17 +96,14 @@ private:
     bool unsaved_changes() const;
     const TriangleMesh* mesh() const;
 
-    bool m_editing_mode = true;            // Is editing mode active?
-    bool m_old_editing_state = false;       // To keep track of whether the user toggled between the modes (needed for imgui refreshes).
-    float m_new_point_head_diameter;        // Size of a new point.
-    float m_new_cone_angle = 0.f;
-    float m_new_cone_height = 5.f;
-    CacheEntry m_point_before_drag;         // undo/redo - so we know what state was edited
-    float m_old_point_head_diameter = 0.;   // the same
+    bool  m_show_supports = true;
+    float m_new_hole_radius;        // Size of a new hole.
+    float m_new_hole_height = 5.f;
+    float m_old_hole_radius = 0.;   // undo/redo - so we know what state was edited
+    Vec3f m_hole_before_drag = Vec3f::Zero();
     float m_minimal_point_distance_stash = 0.f; // and again
     float m_density_stash = 0.f;                // and again
-    mutable std::vector<CacheEntry> m_editing_cache; // a support point and whether it is currently selected
-    std::vector<sla::SupportPoint> m_normal_cache; // to restore after discarding changes or undo/redo
+    mutable std::vector<bool> m_selected; // which holes are currently selected
 
     float m_offset = 2.f;
     float m_adaptability = 1.f;
@@ -148,7 +144,7 @@ protected:
     void on_set_hover_id() override
 
     {
-        if (! m_editing_mode || (int)m_editing_cache.size() <= m_hover_id)
+        if (int(m_model_object->sla_drain_holes.size()) <= m_hover_id)
             m_hover_id = -1;
     }
     void on_start_dragging() override;

src/slic3r/GUI/Gizmos/GLGizmosManager.cpp
@@ -750,20 +750,31 @@ bool GLGizmosManager::on_key(wxKeyEvent& evt)
 
     if (evt.GetEventType() == wxEVT_KEY_UP)
     {
-        if (m_current == SlaSupports)
+        if (m_current == SlaSupports || m_current == Hollow)
         {
-            GLGizmoSlaSupports* gizmo = dynamic_cast<GLGizmoSlaSupports*>(get_current());
+            bool is_editing = true;
+            bool is_rectangle_dragging = false;
+
+            if (m_current == SlaSupports) {
+                GLGizmoSlaSupports* gizmo = dynamic_cast<GLGizmoSlaSupports*>(get_current());
+                is_editing = gizmo->is_in_editing_mode();
+                is_rectangle_dragging = gizmo->is_selection_rectangle_dragging();
+            }
+            else {
+                GLGizmoHollow* gizmo = dynamic_cast<GLGizmoHollow*>(get_current());
+                is_rectangle_dragging = gizmo->is_selection_rectangle_dragging();
+            }
 
             if (keyCode == WXK_SHIFT)
             {
                 // shift has been just released - SLA gizmo might want to close rectangular selection.
-                if (gizmo_event(SLAGizmoEventType::ShiftUp) || (gizmo->is_in_editing_mode() && gizmo->is_selection_rectangle_dragging()))
+                if (gizmo_event(SLAGizmoEventType::ShiftUp) || (is_editing && is_rectangle_dragging))
                     processed = true;
             }
             else if (keyCode == WXK_ALT)
             {
                 // alt has been just released - SLA gizmo might want to close rectangular selection.
-                if (gizmo_event(SLAGizmoEventType::AltUp) || (gizmo->is_in_editing_mode() && gizmo->is_selection_rectangle_dragging()))
+                if (gizmo_event(SLAGizmoEventType::AltUp) || (is_editing && is_rectangle_dragging))
                     processed = true;
             }
         }

commit f66d83a1bd8e6a6f97508622a965334bd8c7c501

Fixed a crash during cut (typo introduced in 6b0de7a)

src/libslic3r/Model.cpp
@@ -1113,7 +1113,7 @@ ModelObjectPtrs ModelObject::cut(size_t instance, coordf_t z, bool keep_upper, b
     if (keep_upper) {
         upper->set_model(nullptr);
         upper->sla_support_points.clear();
-        lower->sla_drain_holes.clear();
+        upper->sla_drain_holes.clear();
         upper->sla_points_status = sla::PointsStatus::NoPoints;
         upper->clear_volumes();
         upper->input_file = "";